6aeed64598ba00670fa8b97ce9dc40ffdc802405
Ricardo Yang <ricardo.yang00@gmail.com>
2025-03-06 11:28:10 +0000

    tp3: cube illumination done

--- tp3/MyUnitCube.js
+++ tp3/MyUnitCube.js
@@ -12,102 +12,95 @@ export class MyUnitCube extends CGFobject {
 
     initBuffers() {
         this.vertices = [
-            -0.5, -0.5, 0.5,    // 0 - Bottom left front
-            0.5, -0.5, 0.5,     // 1 - Bottom right front   
-            0.5, -0.5, -0.5,    // 2 - Bottom right back
-            -0.5, -0.5, -0.5,   // 3 - Bottom left back
-        
+            // bottom
+            0.5, -0.5, 0.5,     // 0 - Bottom right front   
+            -0.5, -0.5, 0.5,    // 1 - Bottom left front
+            -0.5, -0.5, -0.5,   // 2 - Bottom left back
+            0.5, -0.5, -0.5,    // 3 - Bottom right back
+
+            // top
             -0.5, 0.5, 0.5,     // 4 - Top left front
             0.5, 0.5, 0.5,      // 5 - Top right front
             0.5, 0.5, -0.5,     // 6 - Top right back
             -0.5, 0.5, -0.5,    // 7 - Top left back
 
-            -0.5, -0.5, 0.5,    // 8 - Bottom left front
-            0.5, -0.5, 0.5,     // 9 - Bottom right front   
-            0.5, -0.5, -0.5,    // 10 - Bottom right back
-            -0.5, -0.5, -0.5,   // 11 - Bottom left back
-            
-            -0.5, 0.5, 0.5,     // 12 - Top left front
-            0.5, 0.5, 0.5,      // 13 - Top right front
-            0.5, 0.5, -0.5,     // 14 - Top right back
-            -0.5, 0.5, -0.5,    // 15 - Top left back
+            // left
+            -0.5, -0.5, -0.5,   // 8 - Bottom left back
+            -0.5, -0.5, 0.5,    // 9 - Bottom left front
+            -0.5, 0.5, 0.5,     // 10 - Top left front
+            -0.5, 0.5, -0.5,    // 11 - Top left back
 
+            // right
+            0.5, -0.5, 0.5,     // 12 - Bottom right front   
+            0.5, -0.5, -0.5,    // 13 - Bottom right back
+            0.5, 0.5, -0.5,     // 14 - Top right back
+            0.5, 0.5, 0.5,      // 15 - Top right front
+            
+            // front
             -0.5, -0.5, 0.5,    // 16 - Bottom left front
             0.5, -0.5, 0.5,     // 17 - Bottom right front   
-            0.5, -0.5, -0.5,    // 18 - Bottom right back
-            -0.5, -0.5, -0.5,   // 19 - Bottom left back
-            
-            -0.5, 0.5, 0.5,     // 20 - Top left front
-            0.5, 0.5, 0.5,      // 21 - Top right front
-            0.5, 0.5, -0.5,     // 22 - Top right back
-            -0.5, 0.5, -0.5,    // 23 - Top left back
+            0.5, 0.5, 0.5,      // 18 - Top right front
+            -0.5, 0.5, 0.5,     // 19 - Top left front
+
+            // back
+            0.5, -0.5, -0.5,    // 20 - Bottom right back
+            -0.5, -0.5, -0.5,   // 21 - Bottom left back
+            -0.5, 0.5, -0.5,    // 22 - Top left back
+            0.5, 0.5, -0.5      // 23 - Top right back
         ];
+            
 
         this.normals = [
-            -1,0,0,     // 0
-            1,0,0,      // 1
-            1,0,0,      // 2
-            -1,0,0,     // 3
-            -1,0,0,     // 4
-            1,0,0,      // 5
-            1,0,0,      // 6
-            -1,0,0,     // 7
+            0,-1,0,    
+            0,-1,0,    
+            0,-1,0,     
+            0,-1,0,  
 
-            0,-1,0,     // 8
-            0,-1,0,     // 9
-            0,-1,0,     // 10
-            0,-1,0,     // 11
-            0,1,0,      // 12
-            0,1,0,      // 13
-            0,1,0,      // 14
-            0,1,0,      // 15
+            0,1,0,      
+            0,1,0,      
+            0,1,0,      
+            0,1,0,   
 
-            0,0,1,      // 16
-            0,0,1,      // 17
-            0,0,-1,     // 18
-            0,0,-1,     // 19
-            0,0,1,      // 20
-            0,0,1,      // 21
-            0,0,-1,     // 22
-            0,0,-1,     // 23            
+            -1,0,0,    
+            -1,0,0,    
+            -1,0,0,    
+            -1,0,0,   
+
+            1,0,0,     
+            1,0,0,     
+            1,0,0,     
+            1,0,0,  
+
+            0,0,1,      
+            0,0,1,      
+            0,0,1,      
+            0,0,1, 
+
+            0,0,-1,     
+            0,0,-1,     
+            0,0,-1,     
+            0,0,-1
         ];
 
         //Counter-clockwise reference of vertices
         this.indices = [
-            // base
-            1, 0, 3,
-            3, 2, 1,
+            0,1,2,
+            2,3,0,
 
+            4,5,6,
+            6,7,4,
 
+            8,9,10,
+            10,11,8,
 
-            // front
-            4, 0, 1,
-            1, 5, 4,
+            12,13,14,
+            14,15,12,
 
-  
+            16,17,18,
+            18,19,16,
 
-            // right
-            5, 1, 2,
-            2, 6, 5,
-
-
-
-            // back
-            6, 2, 3,
-            3, 7, 6,
-
-
-
-            // left
-            7, 3, 0,
-            0, 4, 7,
-            
-
-            // top
-            7, 4, 5,
-            5, 6, 7,
-
-
+            20,21,22,
+            22,23,20
         ];
 
         //The defined indices (and corresponding vertices)
