29b594d3594c62adde10731f6241e2416513c035
Ricardo Yang <ricardo.yang00@gmail.com>
2025-03-07 22:29:58 +0000

    tp3: wood material created and readme initiated

--- tp3/MyScene.js
+++ tp3/MyScene.js
@@ -129,6 +129,13 @@ export class MyScene extends CGFscene {
         this.material3.setSpecular(1, 0, 0, 1.0);
         this.material3.setShininess(10.0);
 
+        // Wood 
+        this.materialWood = new CGFappearance(this);
+        this.materialWood.setAmbient(0.4, 0.2, 0.1, 1.0);
+        this.materialWood.setDiffuse(0.6, 0.3, 0.2, 1.0);
+        this.materialWood.setSpecular(0.1, 0.1, 0.1, 1.0);
+        this.materialWood.setShininess(10.0);
+
         // Custom material (can be changed in the interface)
         // initially midrange values on ambient, diffuse and specular, on R, G and B respectively
 
@@ -142,10 +149,10 @@ export class MyScene extends CGFscene {
 
         this.updateCustomMaterial();
 
-        this.materials = [this.material1, this.material2, this.material3, this.customMaterial];
+        this.materials = [this.material1, this.material2, this.material3, this.materialWood, this.customMaterial];
 
         // Labels and ID's for object selection on MyInterface
-        this.materialIDs = {'Red Ambient': 0, 'Red Diffuse': 1, 'Red Specular': 2, 'Custom': 3 };
+        this.materialIDs = {'Red Ambient': 0, 'Red Diffuse': 1, 'Red Specular': 2, 'Wood': 3, 'Custom': 4 };
     }
     display() {
         // ---- BEGIN Background, camera and axis setup
--- tp3/README.md
+++ tp3/README.md
@@ -1,1 +1,8 @@
-# [COPY FROM PREVIOUS TP AND ADJUST]
+# CG 2024/2025
+
+## Group T07G02
+
+## TP 3 Notes
+- In the first exercise, the correct normals were set for the cube and tangram to reflect light accurately and naturally. 
+- A wood material was also created, as shown in the image.
+![](screenshots/cg-t07g02-tp3-1.png)
