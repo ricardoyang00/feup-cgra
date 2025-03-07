1b91d30b9581260cdbadf1a1f6e869f78cc26de0
Ricardo Yang <ricardo.yang00@gmail.com>
2025-03-07 22:53:16 +0000

    tp3: part1 done

--- tp3/MyTangram.js
+++ tp3/MyTangram.js
@@ -1,4 +1,4 @@
-import {CGFobject} from '../lib/CGF.js';
+import {CGFobject, CGFappearance} from '../lib/CGF.js';
 import { MyDiamond } from "./MyDiamond.js";
 import { MyParallelogram } from "./MyParallelogram.js";
 import { MyTriangleSmall } from "./MyTriangleSmall.js";
@@ -17,50 +17,97 @@ export class MyTangram extends CGFobject {
         this.scene.parallelogram = new MyParallelogram(this.scene);
         this.scene.triangleSmall = new MyTriangleSmall(this.scene);
         this.scene.triangleBig = new MyTriangleBig(this.scene);
+
+        this.initMaterials();
 	}
+
+    initMaterials() {
+        // materials for the pieces
+        this.greenMaterial = new CGFappearance(this.scene);
+        this.greenMaterial.setAmbient(0, 1, 0, 1);
+        this.greenMaterial.setDiffuse(0, 1, 0, 1);
+        this.greenMaterial.setSpecular(1, 1, 1, 1);
+        this.greenMaterial.setShininess(100.0);
+
+        this.yellowMaterial = new CGFappearance(this.scene);
+        this.yellowMaterial.setAmbient(1, 1, 0, 1);
+        this.yellowMaterial.setDiffuse(1, 1, 0, 1);
+        this.yellowMaterial.setSpecular(1, 1, 1, 1);
+        this.yellowMaterial.setShininess(100.0);
+
+        this.redMaterial = new CGFappearance(this.scene);
+        this.redMaterial.setAmbient(1, 0, 0, 1);
+        this.redMaterial.setDiffuse(1, 0, 0, 1);
+        this.redMaterial.setSpecular(1, 1, 1, 1);
+        this.redMaterial.setShininess(100.0);
+
+        this.purpleMaterial = new CGFappearance(this.scene);
+        this.purpleMaterial.setAmbient(0.5, 0, 0.5, 1);
+        this.purpleMaterial.setDiffuse(0.5, 0, 0.5, 1);
+        this.purpleMaterial.setSpecular(1, 1, 1, 1);
+        this.purpleMaterial.setShininess(100.0);
+
+        this.pinkMaterial = new CGFappearance(this.scene);
+        this.pinkMaterial.setAmbient(1, 0.75, 0.8, 1);
+        this.pinkMaterial.setDiffuse(1, 0.75, 0.8, 1);
+        this.pinkMaterial.setSpecular(1, 1, 1, 1);
+        this.pinkMaterial.setShininess(100.0);
+
+        this.blueMaterial = new CGFappearance(this.scene);
+        this.blueMaterial.setAmbient(0, 0, 1, 1);
+        this.blueMaterial.setDiffuse(0, 0, 1, 1);
+        this.blueMaterial.setSpecular(1, 1, 1, 1);
+        this.blueMaterial.setShininess(100.0);
+
+        this.orangeMaterial = new CGFappearance(this.scene);
+        this.orangeMaterial.setAmbient(1, 0.5, 0, 1);
+        this.orangeMaterial.setDiffuse(1, 0.5, 0, 1);
+        this.orangeMaterial.setSpecular(1, 1, 1, 1);
+        this.orangeMaterial.setShininess(100.0);
+    }
 
     display() {
         this.scene.pushMatrix();
-        this.scene.setDiffuse(0, 1, 0, 1);  // green
+        this.scene.customMaterial.apply();  // custom
         this.scene.translate(3, 0, 0);
         this.scene.diamond.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.setDiffuse(1, 1, 0, 1);  // yellow
+        this.yellowMaterial.apply();        // yellow
         this.scene.rotate(Math.PI, 0, 1, 0);
         this.scene.translate(1, -1, 0);
         this.scene.parallelogram.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.setDiffuse(1, 0, 0, 1);  // red
+        this.redMaterial.apply();           // red
         this.scene.translate(4, -1, 0);
         this.scene.triangleSmall.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.setDiffuse(0.5, 0, 0.5, 1);  // purple
+        this.purpleMaterial.apply();        // purple
         this.scene.rotate(Math.PI, 0, 0, 1);
         this.scene.translate(1, 0, 0);
         this.scene.triangleSmall.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.setDiffuse(1, 0.75, 0.8, 1);  // pink
+        this.pinkMaterial.apply();          // pink
         this.scene.translate(-4.5, -1, 0);
         this.scene.scale(1.5, 1.5, 1);
         this.scene.triangleSmall.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.setDiffuse(0, 0, 1, 1);  // blue
+        this.blueMaterial.apply();          // blue
         this.scene.translate(1, -1, 0);
         this.scene.triangleBig.display();
         this.scene.popMatrix();
 
         this.scene.pushMatrix();
-        this.scene.setDiffuse(1, 0.5, 0, 1);  // orange
+        this.orangeMaterial.apply();        // orange
         this.scene.translate(-2, 0, 0);
         this.scene.triangleBig.display();
         this.scene.popMatrix();
--- tp3/README.md
+++ tp3/README.md
@@ -4,5 +4,12 @@
 
 ## TP 3 Notes
 - In the first exercise, the correct normals were set for the cube and tangram to reflect light accurately and naturally. 
-- A wood material was also created, as shown in the image.
+- A wood material was also created, as shown in the figure 1.
+
 ![](screenshots/cg-t07g02-tp3-1.png)
+<p align="center" justify="center">Figure 1 - Wood cube<br/><br/>
+
+
+- Each tangram piece was assigned a material with the correct color and high specular, as shown in Figure 2. The MyDiamond piece (green) uses a custom material adjustable via the interface.
+![](screenshots/cg-t07g02-tp3-2.png)
+<p align="center" justify="center">Figure 2 - MyTangram colored<br/><br/>
