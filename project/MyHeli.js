d643ac2b0e8c037569e7186ad721cd4e0f6ce38b
Bruno Huang <up202207517@edu.fe.up.pt>
2025-05-24 18:50:48 +0100

    heli: added the over lake and fire indicators condition and added indexes to camera views to avoid heli movement to change camera view

--- project/MyHeli.js
+++ project/MyHeli.js
@@ -622,17 +622,16 @@ export class MyHeli extends CGFobject {
         if (this.state === "taking_off" || this.state === "ascending_from_lake") {
             heliState = "up";
         } else if (this.state === "landing" || this.state === "reorienting_to_land" || 
-                  this.state === "descending_to_lake" || this.state === "retracting_bucket") {
+            this.state === "descending_to_lake" || this.state === "retracting_bucket") {
             heliState = "down";
         }
-        // All other states have no indicator (null)
         
-        // TODO : lake/fire detection
-        // if (this.scene.isOverLake(this.getWorldPosition())) {
-        //     heliState = "over_lake";
-        // } else if (this.scene.isOverFire(this.getWorldPosition())) {
-        //     heliState = "over_fire";
-        // }
+        // Set over lake/fire indicator
+        if (this.scene.isOverLake(this.getWorldPosition())) {
+            heliState = "over_lake";
+        } else if (this.scene.isOverFire(this.getWorldPosition())) {
+            heliState = "over_fire";
+        }
         
         // Set water status indicator
         if (this.bucketIsEmpty) {
